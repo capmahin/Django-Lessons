b2809a18b7e0fc3d39d0f26228537f00af80a481
unknown <mahinulhuque1996@gmail.com>
2025-10-01 20:29:27 +0600

    lesson

---
+++ Lession3-MigrateSQLite3/worldtour/asiatoursagency/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Tour(models.Model):
+    # We need a origin country , we need destination,number of nights, and we need the price for that tour. 
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
