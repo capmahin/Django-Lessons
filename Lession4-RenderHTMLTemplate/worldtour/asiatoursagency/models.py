fdbded9bd80e98e017319a72e28e060f1a8606f2
unknown <mahinulhuque1996@gmail.com>
2025-10-05 12:54:09 +0600

    add another template

---
+++ Lession4-RenderHTMLTemplate/worldtour/asiatoursagency/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Tour(models.Model):
+    # We need a origin country , we need destination,number of nights, and we need the price for that tour. 
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
+    
+    #This is a string representation of the tours
+    def __str__(self):
+        return (f"ID:{self.id}: From {self.origin_country} To {self.destination_country},{self.number_of_nights} nights costs ${self.price}")
